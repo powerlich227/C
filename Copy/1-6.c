070c17f5e741977659b96dd8fa69bb3dec5c987f
powerlich227 <yemingxiao1990@gmail.com>
2017-12-22 21:34:24 -0500

    First Commit

---
+++ Copy/1-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	int c;
+
+	while (c = getchar() != EOF) {
+		printf("%d\n", c);
+	}
+	printf("%d - at EOF\n", c);
+}
---
+++ Copy/1-7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("EOF is %d\n", EOF);
+}
